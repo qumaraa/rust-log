6a539964a533728175a8d46e9f9e6f879e460e16
Alexander <112755279+alexanderqmv@users.noreply.github.com>
2023-07-11 05:29:09 -0700

    Update mod.rs

--- src/mod.rs
+++ src/mod.rs
@@ -1,8 +1,37 @@
+//!
+//! # Rust-Log: A simple and flexible Logging System for Rust.
+//! We present you a simple but flexible logging system with many different levels of logging.
+//!
+//! [LogLevel] - structure containing logging levels
+//!
+//! [Logger] - a structure containing functions for logging.
+//!
+//! # Example
+//! ```
+//!fn main() {
+//!   use rust_log::{Logger, LogLevel};
+//!
+//!   let logger = Logger::new(LogLevel::Debug, "c:/users/user/desktop/my.txt", true);
+//!
+//!   // use macros (more comfortable)
+//!
+//!   info!(logger, "This is an info message!");
+//!   /*...*/
+//!}
+//!
+//! ```
+//!
+//!
+#[macro_use]
+#[macro_export]
 extern crate colored;
-use colored::*;
-use chrono::Utc;
 
-#[derive(Copy, Clone)]
+use std::fs;
+use std::io::Write;
+use chrono::Utc;
+use colored::*;
+
+#[derive(Copy, Clone,PartialEq)]
 pub enum LogLevel {
     Debug,
     Info,
@@ -12,44 +41,64 @@ pub enum LogLevel {
     Critical,
 }
 
-
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone,PartialEq)]
 pub struct Logger {
     level: LogLevel,
+    path: &'static str,
+    save: bool,
 }
 
 impl Logger {
-    pub fn new(level: LogLevel) -> Self {
-        Logger { level }
+    pub fn new(level: LogLevel, path: &'static str, save: bool) -> Self {
+        Logger { level, path, save }
     }
 
-    fn log(&self, level: LogLevel, message: &str) {
-        if level as u8 >= self.level as u8 {
+    fn log(&mut self, level: LogLevel, message: &str) {
+        if level as u8 >= self.level as u8 && level != LogLevel::Error {
             let now = Utc::now();
-            println!("[{}] {} - {}", level_to_string(level.clone()),now,message);
+            let msg = format!("[{}] {} - {}\n", level_to_string(level.clone()), now, message);
+            println!("{msg}");
+            if self.save {
+                self.save_log(msg.as_str());
+            }
+        }else {
+            let now = Utc::now();
+            let msg = format!("[{}] {} - {}\n", level_to_string(level.clone()), now, message);
+            println!("{msg}");
+            if self.save {
+                self.save_log(msg.as_str());
+            }
         }
     }
-
-    pub fn debug(&self, message: &str) {
+    fn save_log(&mut self, data: &str) {
+        let mut op = fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .append(true)
+            .open(self.path).unwrap();
+        op.write_all(data.as_bytes()).unwrap();
+    }
+    pub fn debug(&mut self, message: &str) {
         self.log(LogLevel::Debug, message);
     }
 
-    pub fn info(&self, message: &str) {
+    pub fn info(&mut self, message: &str) {
         self.log(LogLevel::Info, message);
     }
 
-    pub fn error(&self, message: &str) {
+    pub fn error(&mut self, message: &str) {
         self.log(LogLevel::Error, message);
     }
 
-    pub fn warn(&self, message: &str) {
+    pub fn warn(&mut self, message: &str) {
         self.log(LogLevel::Warn, message);
     }
 
-    pub fn fatal(&self, message: &str) {
+    pub fn fatal(&mut self, message: &str) {
         self.log(LogLevel::Fatal, message);
     }
-    pub fn critical(&self, message: &str) {
+
+    pub fn critical(&mut self, message: &str) {
         self.log(LogLevel::Critical, message);
     }
 }
@@ -100,3 +149,5 @@ fn level_to_string(level: LogLevel) -> String {
         LogLevel::Critical => format!("{}", "CRITICAL".bright_red().bold().italic().underline()),
     }
 }
+
+
